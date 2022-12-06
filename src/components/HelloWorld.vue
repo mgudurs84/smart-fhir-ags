2d07f0998d2c9fffa851a752d80fcf223ab79e13
mgudurs84 <35053619+mgudurs84@users.noreply.github.com>
2022-12-06 12:08:40 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -75,7 +75,7 @@ export default {
       patientdata: {},
         clientId: "5e38365a-6b1f-4fd1-ad1a-70569e51683a", // Replace with your client id
       redirect: import.meta.env.PROD
-        ? "https://lucid-wozniak-940eae.netlify.app"
+        ? "https://main.d3hokjthccpmzg.amplifyapp.com"
         : "https://main.d3hokjthccpmzg.amplifyapp.com",
     };
   },
